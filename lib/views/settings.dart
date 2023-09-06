bebb6f12f7788f2d5ea73d29388ecced00bf7fad
Divyam <divyamgupta2001@gmail.com>
2023-09-06 20:46:27 +0530

    updation working

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -274,7 +274,7 @@ class _SettingsPageState extends State<SettingsPage> {
                     localConnectStatus = localConnectStatus.substring(
                         1, localConnectStatus.length - 1);
                     for (var element in locks) {
-                      if (localConnectStatus == (element.lockSSID)) {
+                      if ("BBTLOCK11" == (element.lockSSID)) {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
